4dd32cefbdd22503f0ae6d19aee15bba04ed39ba
Wajiha Kanwal <waji.kanwal@yahoo.com>
2016-02-12 17:28:45 +0500

    Class code & lab assignments

---
+++ JS_ClassCode/Lecture11-December13,2015/DemoJS.js
@@ -0,0 +1,10 @@
+        function tellTime()
+        {
+            var current = new Date();
+            var hours   = current.getHours();
+            var minutes = current.getMinutes();
+            document.write("Current time: " + (hours + ":" + minutes));
+        }
+
+        var country = "Pakistan";
+        alert("Pakistan");
---
+++ JS_ClassCode/Lecture11-December13,2015/SectionB/DemoJS.js
@@ -0,0 +1,15 @@
+
+//This displays current time in HH:MM format
+
+/*
+function tellTime()
+{
+       var current = new Date();
+       var hours   = current.getHours();
+       var minutes = current.getMinutes();
+       document.write("Current time: " + (hours + ":" + minutes));
+}
+*/
+
+var country = "Pakistan";
+alert(country);
